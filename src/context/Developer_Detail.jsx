ca49667afa0b42e96107734718247ecd03c93538
TKM_KraSuang <46192614+KraSuang@users.noreply.github.com>
2023-10-19 11:35:13 +0000

    v1.0.1 Hutao

--- src/context/Developer_Detail.jsx
+++ src/context/Developer_Detail.jsx
@@ -8,7 +8,7 @@ export default function Developer_Detail() {
             <p className="text-lg my-4">Tools</p>
             <p className="text-lg px-4">- Vite | Build Tools <br /> - pnpm | Package Management <br /> - npm | Package Management</p>
             <p className="text-lg my-4">Dependencies</p>
-            <p className="text-lg px-4">- React | Framework | {data.dependencies.react} <br /> - react-dom | RouterDom | {data.dependencies["react-dom"]} <br /> - react-icons | Icon | {data.dependencies["react-icons"]} <br /> - react-router-dom | RouterDom | {data.dependencies["react-router-dom"]} <br /> - TailwindCSS | CSS Framework | {data.devDependencies.tailwindcss}</p>
+            <p className="text-lg px-4">- React | Framework | {data.dependencies.react} <br /> - react-dom | RouterDom | {data.dependencies["react-dom"]} <br /> - react-icons | Icon | {data.dependencies["react-icons"]} <br /> - react-router-dom | RouterDom | {data.dependencies["react-router-dom"]} <br /> - TailwindCSS | CSS Framework | {data.devDependencies.tailwindcss} <br/> - @headlessui/react | Components | {data.dependencies["@headlessui/react"]}</p>
             <p className="text-lg my-4">DevDependencies</p>
             <p className="text-lg px-4">@types/react ^18.2.15 <br />@types/react-dom ^18.2.7<br /> @vitejs/plugin-react ^4.1.0<br /> autoprefixer ^10.4.16<br /> eslint ^8.45.0<br /> eslint-plugin-react ^7.32.2<br /> eslint-plugin-react-hooks ^4.6.0<br /> eslint-plugin-react-refresh ^0.4.3<br /> postcss ^8.4.31<br /> tailwindcss ^3.3.3 <br />vite ^4.4.5</p>
 
